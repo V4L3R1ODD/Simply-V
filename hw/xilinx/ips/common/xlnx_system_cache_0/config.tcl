dc768299432ce4b8de1ecb9509bbc31a22e45f45
Valedd99 <didomenico.valerio@virgilio.it>
2025-09-24 11:18:51 +0200

    Fixed system_cache config file

--- hw/xilinx/ips/common/xlnx_system_cache_0/config.tcl
+++ hw/xilinx/ips/common/xlnx_system_cache_0/config.tcl
@@ -19,45 +19,46 @@ set_property -dict [list \
   CONFIG.C_CCIX0_CACHE_LINE_SIZE {128} \
   CONFIG.C_M0_AXI_THREAD_ID_WIDTH $::env(MBUS_ID_WIDTH) \
   CONFIG.C_M0_AXI_ADDR_WIDTH $::env(MBUS_ADDR_WIDTH) \
-# CONFIG.C_ENABLE_COHERENCY {0} \
-# CONFIG.C_ENABLE_SLAVE_COHERENCY {0} \
-# CONFIG.C_ENABLE_MASTER_COHERENCY {0} \
-# CONFIG.C_ENABLE_ACE_PROTOCOL {0} \
-# CONFIG.C_ENABLE_CCIX_PROTOCOL {0} \
-# CONFIG.C_ENABLE_CHI_PROTOCOL {0} \
-# CONFIG.C_ENABLE_INTEGRITY {0} \
-# CONFIG.C_ENABLE_NON_SECURE {0} \
-# CONFIG.C_ENABLE_ERROR_HANDLING {0} \
-# CONFIG.C_ENABLE_CTRL {0} \
-# CONFIG.C_ENABLE_STATISTICS {0} \
-# CONFIG.C_ENABLE_VERSION_REGISTER {0} \
-# CONFIG.C_ENABLE_INTERRUPT {0} \
-# CONFIG.C_ENABLE_ADDRESS_TRANSLATION {0} \
-# CONFIG.C_NUM_WAYS {2} \
-# CONFIG.C_NUM_SLAVE_TRANSACTIONS {16} \
-# CONFIG.C_NUM_MASTER_TRANSACTIONS {32} \
-# CONFIG.C_NUM_SNOOP_TRANSACTIONS {32} \
-# CONFIG.C_NUM_OOO_CHANNELS {0} \
-# CONFIG.C_CACHE_DATA_WIDTH {512} \
-# CONFIG.C_CACHE_TAG_MEMORY_TYPE {0} \
-# CONFIG.C_CACHE_DATA_MEMORY_TYPE {0} \
-# CONFIG.C_CACHE_LRU_MEMORY_TYPE {0} \
-# CONFIG.C_Lx_CACHE_LINE_LENGTH {4} \
-# CONFIG.C_Lx_CACHE_SIZE {8192} \
-# CONFIG.C_SUPPORT_SNOOP_FILTER {0} \
-# CONFIG.C_SNOOP_KEEP_READ_ONCE {1} \
-# CONFIG.C_SNOOP_KEEP_READ_SHARED {0} \
-# CONFIG.C_SNOOP_KEEP_READ_CLEAN {0} \
-# CONFIG.C_SNOOP_KEEP_READ_NSD {0} \
-# CONFIG.C_SNOOP_KEEP_CLEAN_SHARED {0} \
-# CONFIG.C_SNOOP_KEEP_SNPTOSC {0} \
-# CONFIG.C_SNOOP_PASS_READ_ONCE {0} \
-# CONFIG.C_SNOOP_PASS_READ_SHARED {0} \
-# CONFIG.C_SNOOP_PASS_READ_CLEAN {0} \
-# CONFIG.C_SNOOP_PASS_READ_NSD {0} \
-# CONFIG.C_DEFAULT_DOMAIN {0} \
-# CONFIG.C_ENABLE_FAST_INIT_SIM {0} \
 ] [get_ips $::env(IP_NAME)]
 
-# set_property CONFIG.C_M0_AXI_THREAD_ID_WIDTH  $::env(MBUS_ID_WIDTH)       [get_ips $::env(IP_NAME)]
-# set_property CONFIG.C_M0_AXI_ADDR_WIDTH $::env(MBUS_ADDR_WIDTH)  [get_ips $::env(IP_NAME)]
+
+# Options available but not enabled
+# set_property CONFIG.C_ENABLE_COHERENCY {0}            [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_ENABLE_SLAVE_COHERENCY {0}      [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_ENABLE_MASTER_COHERENCY {0}     [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_ENABLE_ACE_PROTOCOL {0}         [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_ENABLE_CCIX_PROTOCOL {0}        [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_ENABLE_CHI_PROTOCOL {0}         [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_ENABLE_INTEGRITY {0}            [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_ENABLE_NON_SECURE {0}           [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_ENABLE_ERROR_HANDLING {0}       [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_ENABLE_CTRL {0}                 [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_ENABLE_STATISTICS {0}           [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_ENABLE_VERSION_REGISTER {0}     [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_ENABLE_INTERRUPT {0}            [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_ENABLE_ADDRESS_TRANSLATION {0}  [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_NUM_WAYS {2}                    [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_NUM_SLAVE_TRANSACTIONS {16}     [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_NUM_MASTER_TRANSACTIONS {32}    [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_NUM_SNOOP_TRANSACTIONS {16}     [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_NUM_OOO_CHANNELS {0}            [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_CACHE_DATA_WIDTH {512}          [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_CACHE_TAG_MEMORY_TYPE {0}       [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_CACHE_DATA_MEMORY_TYPE {0}      [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_CACHE_LRU_MEMORY_TYPE {0}       [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_Lx_CACHE_LINE_LENGTH {4}        [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_Lx_CACHE_SIZE {8192}            [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_SUPPORT_SNOOP_FILTER {0}        [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_SNOOP_KEEP_READ_ONCE {1}        [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_SNOOP_KEEP_READ_SHARED {0}      [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_SNOOP_KEEP_READ_CLEAN {0}       [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_SNOOP_KEEP_READ_NSD {0}         [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_SNOOP_KEEP_CLEAN_SHARED {0}     [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_SNOOP_KEEP_SNPTOSC {0}          [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_SNOOP_PASS_READ_ONCE {0}        [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_SNOOP_PASS_READ_SHARED {0}      [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_SNOOP_PASS_READ_CLEAN {0}       [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_SNOOP_PASS_READ_NSD {0}         [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_DEFAULT_DOMAIN {0}              [get_ips $::env(IP_NAME)]
+# set_property CONFIG.C_ENABLE_FAST_INIT_SIM {0}        [get_ips $::env(IP_NAME)]
+
